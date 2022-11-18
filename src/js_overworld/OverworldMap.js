2d0192db23613b3bae666c168417ce3e3976cacb
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-11-18 21:50:21 +0800

    Add files via upload

--- src/js_overworld/OverworldMap.js
+++ src/js_overworld/OverworldMap.js
@@ -82,13 +82,13 @@ window.OverworldMaps = {
         upperSrc: "./images/maps/DemoUpper.png",
         gameObjects: {
             hero: new Person({
-                playerToken: "player1",
+                playerToken: client_name,
                 isPlayerControlled: true,
                 x: utils.withGrid(5),
                 y: utils.withGrid(6)
             }),
             npc1: new Person({
-                playerToken: "player2",
+                playerToken: behavior.player,
                 isPlayerControlled: false,
                 x: utils.withGrid(7),
                 y: utils.withGrid(9),
